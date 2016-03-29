044606d4ce13514bfee87db160e2d9e229ce3111
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-03-29 14:54:45 +0200

    sql style in tables

--- _sql/01 - create tables.sql
+++ _sql/01 - create tables.sql
@@ -1,30 +1,30 @@
 -- tabuľka pre užívateľov
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name character varying(100) NOT NULL,
-    email character varying(100) NOT NULL UNIQUE,
-    password character(128) NOT NULL,
-    is_admin boolean DEFAULT false NOT NULL
+  id       SERIAL PRIMARY KEY,
+  name     CHARACTER VARYING(100) NOT NULL,
+  email    CHARACTER VARYING(100) NOT NULL UNIQUE,
+  password CHARACTER(128)         NOT NULL,
+  is_admin BOOLEAN DEFAULT FALSE  NOT NULL
 );
 
 -- tabuľka pre články
 CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    user_id integer NOT NULL REFERENCES users(id),
-    title character varying(100) NOT NULL,
-    text text NOT NULL,
-    has_image BOOLEAN DEFAULT FALSE NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
+  id         SERIAL PRIMARY KEY,
+  user_id    INTEGER                                   NOT NULL REFERENCES users (id),
+  title      CHARACTER VARYING(100)                    NOT NULL,
+  text       TEXT                                      NOT NULL,
+  has_image  BOOLEAN DEFAULT FALSE                     NOT NULL,
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
 );
 
 -- tabuľka pre tagy
 CREATE TABLE tags (
-    id SERIAL PRIMARY KEY,
-    tag character varying(100) NOT NULL UNIQUE
+  id  SERIAL PRIMARY KEY,
+  tag CHARACTER VARYING(100) NOT NULL UNIQUE
 );
 
 -- tabuľka pre spojenie článku s tagmi
 CREATE TABLE posts_tags (
-    post_id integer NOT NULL REFERENCES posts(id),
-    tag_id integer NOT NULL REFERENCES tags(id)
+  post_id INTEGER NOT NULL REFERENCES posts (id),
+  tag_id  INTEGER NOT NULL REFERENCES tags (id)
 );
